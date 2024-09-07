0a9598b3526130b7f8555059434fca0b34042ec2
danielzapatta <vivadanielzapata@Hotmail.com>
2024-09-07 00:50:26 -0300

    Finalizando teste 1

--- src/main/java/testes/target/TesteTarget01.java
+++ src/main/java/testes/target/TesteTarget01.java
@@ -1,2 +1,18 @@
-package testes.target;public class TesteTarget01 {
+package testes.target;
+
+public class TesteTarget01 {
+    public static void main(String[] args) {
+
+        int indice = 12, soma = 0, K = 1;
+
+        while (K < indice) {
+            K = K +1; soma = soma + K;
+
+            System.out.println(soma);
+        }
+
+        // No final do processo, é notado que o valor de soma é 77 pois
+        // A cada iteração, o valor de K é incrementado em 1 e somado ao valor de soma.
+
+    }
 }
